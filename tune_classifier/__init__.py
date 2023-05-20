b5d538ef6659bbfbabe4e2d9d002dd897fc14de8
Darkshades <henrychibueze774@gmail.com>
2023-05-20 08:23:40 +0100

    submodule renaming and __init__.py file config

---
+++ tune_classifier/__init__.py
@@ -0,0 +1,11 @@
+import inspect
+from .models import *
+from types import ModuleType
+from typing import Iterable, Tuple, Dict
+
+TARGET_MODULE: str = "tune_classifier.models"
+module: ModuleType = __import__(TARGET_MODULE)
+_entities: Iterable[Tuple[str, object]] = inspect.getmembers(module)
+_classes: object = filter(lambda entity : inspect.isclass(object=entity[1]), _entities)
+tuning_entities: object = filter(lambda entity : entity[1].__module__ == TARGET_MODULE, _classes)
+tuning_entities: Dict = dict(tuning_entities)
