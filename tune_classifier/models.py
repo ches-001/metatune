7369941f6c6ac5f90117074d617d8e0880da909f
David Onyeali <onyealidavidcn@gmail.com>
2023-05-20 11:47:56 +0100

    added DecisionTree Classifier and Regressor models

--- tune_classifier/models.py
+++ tune_classifier/models.py
@@ -10,6 +10,55 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import BernoulliNB
 
+
+@dataclass
+class DecisionTreeClassifierModel(SampleClassMixin):
+    criterion: Iterable[str] = ("gini", "entropy", "log_loss")
+    splitter: Iterable[str] = ("best", "random")
+    max_depth: Iterable[int] = (2, 1000)
+    min_samples_split: Iterable[int] = (2, 1000)
+    min_samples_leaf: Iterable[int] = (1, 1000)
+
+    # these could also be floats, I'm not sure how to handle them
+    # min_samples_split: Iterable[float] = (2, 9)
+    # min_samples_leaf: Iterable[float] = (1, 9)
+    
+    min_weight_fraction_leaf: Iterable[float] = (0.0, 0.5)
+    max_features: Iterable[str] = ("auto", "sqrt", "log2")
+    max_leaf_nodes: Iterable[int] = (1, 1000)
+    min_impurity_decrease: Iterable[float] = (0.0, 1.0)
+    ccp_alpha: Iterable[float] = (0.0, 1.0)
+    model: Any = None
+    
+    def _sample_params(self, trial: Optional[Trial]=None) -> Dict[str, Any]:
+        super()._sample_params(trial)
+        
+        params = {}
+        params["criterion"] = trial.suggest_categorical("criterion", self.criterion)
+        params["splitter"] = trial.suggest_categorical("splitter", self.splitter)
+        params["max_depth"] = trial.suggest_int("max_depth", *self.max_depth, log=False)
+        params["min_samples_split"] = trial.suggest_int("min_samples_split", *self.min_samples_split, log=False)
+        params["min_samples_leaf"] = trial.suggest_int("min_samples_leaf", *self.min_samples_leaf, log=False)
+        params["min_weight_fraction_leaf"] = trial.suggest_float("min_weight_fraction_leaf", *self.min_weight_fraction_leaf, log=False)
+        params["max_features"] = trial.suggest_categorical("max_features", self.max_features)
+        params["max_leaf_nodes"] = trial.suggest_int("max_leaf_nodes", *self.max_leaf_nodes, log=False)
+        params["min_impurity_decrease"] = trial.suggest_float("min_impurity_decrease", *self.min_impurity_decrease, log=False)
+        params["ccp_alpha"] = trial.suggest_float("ccp_alpha", *self.ccp_alpha, log=False)
+        return params
+    
+    def sample_model(self, trial: Optional[Trial]=None) -> Any:
+        super().model(trial)
+        
+        params = self._sample_params(trial)
+        model = DecisionTreeClassifier(
+            **params,
+            class_weight="balanced",
+            shrinking=True)
+        
+        self.model = model
+        return model
+    
+
 @dataclass
 class SVCModel(SampleClassMixin):
     kernel_space: Iterable[str] = ("linear", "poly", "rbf", "sigmoid")
@@ -39,9 +88,7 @@ class SVCModel(SampleClassMixin):
         params = self._sample_params(trial)
         model = SVC(
             **params, 
-            class_weight="balanced",
-            shrinking=True, 
-            probability=True)
+            class_weight="balanced",)
         
         self.model = model
         return model
