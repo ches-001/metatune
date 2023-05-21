155ef60843a4e688ac32d0c8052c654c6d782ec2
Darkshades <henrychibueze774@gmail.com>
2023-05-21 13:27:18 +0100

    edit on __init__.py of tune_classifier and tune_regressor submodule

--- tune_regressor/__init__.py
+++ tune_regressor/__init__.py
@@ -2,7 +2,7 @@ import inspect
 from tune_regressor.svr import SVRModel
 from tune_regressor.decisiontree import DecisionTreeRegressorModel
 from types import ModuleType
-from typing import Iterable, Tuple, Dict
+from typing import Iterable, Tuple, Dict, Generator
 
 __all__: Iterable[str] = [
     "tune_regressor.svr",
@@ -15,8 +15,9 @@ def get_entities(target_module: str) -> Iterable[Tuple[str, object]]:
     _classes: object = filter(lambda entity : inspect.isclass(object=entity[1]), _entities)
     tuning_entities: object = filter(lambda entity : entity[1].__module__ == target_module, _classes)
     tuning_entities: Iterable[Tuple[str, object]] = list(tuning_entities)
+    tuning_entities: Iterable[Tuple[str, object]] = [(name, _class()) for name, _class in tuning_entities]
 
     return tuning_entities
 
-tuning_entities = list(map(get_entities, __all__))
-tuning_entities = dict(sum(tuning_entities, []))
+tuning_entities: Iterable[Iterable[Tuple[str, object]]] = list(map(get_entities, __all__))
+tuning_entities: Generator = (i for i in sum(tuning_entities, []))
