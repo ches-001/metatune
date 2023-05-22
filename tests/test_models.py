70eaa4ff5731701b3bbdc71fb601eba4a22a690e
David Onyeali <onyealidavidcn@gmail.com>
2023-05-22 11:51:58 +0100

    added ExtraTree models and reordered model test classes

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,16 +7,7 @@ from tests.utils import BaseTest
 # run python -m pytest -v
 
 
-class TestSVR(BaseTest):
-    model: SampleClassMixin = tune_regressor.SVRModel
-    task: str = "regression"
-
-
-class TestDecisionTreeRegressor(BaseTest):
-    model: SampleClassMixin = tune_regressor.DecisionTreeRegressorModel
-    task: str = "regression"
-
-
+# classification tests
 class TestSVC(BaseTest):
     model: SampleClassMixin = tune_classifier.SVCModel
     task: str = "classification"
@@ -25,6 +16,32 @@ class TestSVC(BaseTest):
 class TestDecisionTreeClassifier(BaseTest):
     model: SampleClassMixin = tune_classifier.DecisionTreeClassifierModel
     task: str = "classification"
+
+
+class TestLogisticRegressor(BaseTest):
+    model: SampleClassMixin = tune_classifier.LogisticRegressionModel
+    task: str = "classification"
+
+
+class TestLinearSVC(BaseTest):
+    model: SampleClassMixin = tune_classifier.LinearSVCModel
+    task: str = "classification"
+
+
+class TestExtraTreeClassifier(BaseTest):
+    model: SampleClassMixin = tune_classifier.ExtraTreeClassifierModel
+    task: str = "classification"
+
+
+# regression tests
+class TestSVR(BaseTest):
+    model: SampleClassMixin = tune_regressor.SVRModel
+    task: str = "regression"
+
+
+class TestDecisionTreeRegressor(BaseTest):
+    model: SampleClassMixin = tune_regressor.DecisionTreeRegressorModel
+    task: str = "regression"
 
 
 class TestLinearRegressor(BaseTest):
@@ -42,16 +59,13 @@ class TestRidgeRegressor(BaseTest):
     task: str = "regression"
 
 
-class TestLogisticRegressor(BaseTest):
-    model: SampleClassMixin = tune_classifier.LogisticRegressionModel
-    task: str = "classification"
-
-
-class TestLinearSVC(BaseTest):
-    model: SampleClassMixin = tune_classifier.LinearSVCModel
-    task: str = "classification"
-
-
 class TestLinearSVR(BaseTest):
     model: SampleClassMixin = tune_regressor.LinearSVRModel
     task: str = "regression"
+
+
+class TestExtraTreeRegressor(BaseTest):
+    model: SampleClassMixin = tune_regressor.ExtraTreeRegressorModel
+    task: str = "regression"
+
+
