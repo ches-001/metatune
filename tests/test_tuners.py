0d9c95eaf73a23ba3953d82c09dd9a78caedd93c
David Onyeali <56873169+David2CN@users.noreply.github.com>
2023-05-27 10:30:21 +0100

    Updated test_tuners.py to reflect change in mlp batch_size_space

--- tests/test_tuners.py
+++ tests/test_tuners.py
@@ -188,15 +188,15 @@ class TestSGDRegressorTuner(BaseTest):
 
 
 class TestMLPClassifierTuner(BaseTest):
-    model: SampleClassMixin = tune_classifier.MLPClassifierTuner(batch_size_space=("auto",))
+    model: SampleClassMixin = tune_classifier.MLPClassifierTuner()
     task: str = "classification"
-    n_trials: int = 10
+    n_trials: int = 15
     
 
 class TestMLPRegressorTuner(BaseTest):
-    model: SampleClassMixin = tune_regressor.MLPRegressorTuner(batch_size_space=("auto",))
+    model: SampleClassMixin = tune_regressor.MLPRegressorTuner()
     task: str = "regression"
-    n_trials: int = 10
+    n_trials: int = 15
 
       
 class TestHistGradientBoostingClassifier(BaseTest):
