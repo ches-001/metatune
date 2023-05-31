cadbdb70f8677cdaf0a84dafaa0960f70a2bb6e8
Darkshades <henrychibueze774@gmail.com>
2023-05-31 20:27:29 +0100

    TheilSanRegressorTuner parameter update

--- tune_regressor/linear_model_regressor.py
+++ tune_regressor/linear_model_regressor.py
@@ -691,7 +691,7 @@ class HuberRegressorTuner(BaseTuner):
 class TheilSenRegressorTuner(BaseTuner):
     fit_intercept_space: Iterable[bool] = (True, False)
     max_subpopulation_space: Dict[str, Any] = MappingProxyType({"low":100, "high":1e5, "step":1, "log":True})
-    set_n_subsamples_space: Iterable[bool] = (True, False)
+    set_n_subsamples_space: Iterable[bool] = (False, )
     n_subsamples_space: Optional[Dict[str, Any]] = MappingProxyType({"low":1, "high":40, "step":1, "log":True})
     max_iter_space: Dict[str, Any] = MappingProxyType({"low":100, "high":300, "step":1, "log":True})
     tol_space: Dict[str, Any] = MappingProxyType({"low":1e-6, "high":1e-3, "step":None, "log":True})
